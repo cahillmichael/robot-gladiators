6605df61281717ac8608a70cd60a234f3a11b32b
michaelc <michaelc@carimus.com>
2021-11-07 18:18:01 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,11 +15,11 @@ var fight = function(enemyName) {
     //if player picks "skip", confirm and then stop loop
     if (promptFight === "SKIP" || promptFight === "skip" || promptFight === "Skip") {
         //confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        var confirmSkip = window.confirm("Are you sure you'd like to skip this round?");
 
         //if yes (true), leave fight
         if (confirmSkip) {
-            window.alert(playerName + " has decided to skip the fight. Goodbye!");
+            window.alert(playerName + " has decided to skip the fight.");
             //subtract money from playerMoney for skipping
             playerMoney = playerMoney - 10;
             console.log("playerMoney", playerMoney);
@@ -57,18 +57,52 @@ var fight = function(enemyName) {
     }
 };
 
-for(var i=0; i < enemyNames.length; i++) {
+//function to start a new game
+var startGame = function() {
+    debugger;
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for(var i=0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            //let player know what round they're in
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+            //pick a new enemy based on index of enemyNames
+            var pickedEnemyName = enemyNames[i];
+            //reset enemyHealth
+            enemyHealth = 50;
+            //fight
+            fight(pickedEnemyName);
+        } else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }      
+    }
+    //after loop either playerHealth is 0 or there are no enemies remaining, call endGame
+    endGame();
+};
+
+// function to end the entire game
+var endGame = function() {
+    //if player is still alive, player wins
     if (playerHealth > 0) {
-        //let player know what round they're in
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
-        //pick a new enemy based on index of enemyNames
-        var pickedEnemyName = enemyNames[i];
-        //reset enemyHealth
-        enemyHealth = 50;
-        //fight
-        fight(pickedEnemyName);
+        window.alert("You're the king of the world")
+    }
+    //if player has died, player loses
+    else {
+        window.alert("You dead.")
+    }
+    //ask player if they want to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        //restart game
+        startGame();
     } else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
-    }      
-}
+        window.alert("Goodbye.")
+    }
+};
+
+//start game when page loads
+startGame();
